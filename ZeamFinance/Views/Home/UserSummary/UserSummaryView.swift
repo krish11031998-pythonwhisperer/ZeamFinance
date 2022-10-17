ccb12165086a3adad4b85acdabc639c6f90350b6
Krishna Venkatramani (Rain Work GPGPassphrase) <krishna@rain.bh>
2022-10-17 14:33:00 +0400

    Added Quick Actions

--- ZeamFinance/Views/Home/UserSummary/UserSummaryView.swift
+++ ZeamFinance/Views/Home/UserSummary/UserSummaryView.swift
@@ -17,6 +17,7 @@ class UserInfoView: UIView {
 	private lazy var userBalance: DualLabel = { .init() }()
 	private lazy var mainStack: UIStackView = { .VStack(spacing: 10) }()
 	private lazy var userAssets: UIStackView = { .HStack(spacing: 10) }()
+	private lazy var quickActions: UIStackView = { .VStack(spacing: 8) }()
 	private var addGradient: Bool = false
 	
 	override init(frame: CGRect) {
@@ -38,12 +39,15 @@ class UserInfoView: UIView {
 	}
 	
 	private func setupView() {
-		[userInfo, userBalance].addToView(mainStack)
+		[userInfo, userBalance, userAssets, quickActions].addToView(mainStack)
+		mainStack.setCustomSpacing(25, after: userInfo)
+		mainStack.setCustomSpacing(25, after: userAssets)
 		addSubview(mainStack)
 		setFittingConstraints(childView: mainStack, insets: .init(vertical: 15, horizontal: 10))
 		setupUserInfoView(userName: "Krishna")
 		setupBalanceLabel()
 		setupAssets()
+		setupQuickAction()
 		backgroundColor = .clear
 		clippedCornerRadius = 20
 	}
@@ -67,22 +71,33 @@ class UserInfoView: UIView {
 	}
 	
 	private func setupBalanceLabel() {
-		let label = DualLabel()
 		let balanceText: RenderableText = String(format: "$ %.2f", Float.random(in: 100..<1000)).bold(color: .textColorInverse, size: 30)
-		label.configureLabel(title:"Your Balance".medium(color: .textColorInverse, size: 12) ,
+		userBalance.configureLabel(title:"Your Balance".medium(color: .textColorInverse, size: 12) ,
 							 subTitle: balanceText)
-		mainStack.addArrangedSubview(label)
 	}
 	
 	private func setupAssets() {
 		userAssets.alignment = .center
+		userAssets.removeChildViews()
 		[UIImage.IconCatalogue.coins, UIImage.IconCatalogue.vouchers].forEach {
 			let view = ProfileAsset()
 			view.configureView(img: $0.image, text: "\(Int.random(in: 100..<10000))")
 			userAssets.addArrangedSubview(view)
 		}
 		userAssets.addArrangedSubview(.spacer())
-		mainStack.addArrangedSubview(userAssets)
+	}
+	
+	private func setupQuickAction() {
+		let label = "Quick Actions".medium(color: .textColorInverse, size: 12).generateLabel
+		let stack = UIStackView.HStack(spacing: 8)
+		stack.alignment = .center
+		["Receive", "Sell", "Pay"].map {
+			let view = $0.medium(size: 12).generateLabel
+			return view.blobify(backgroundColor: .surfaceBackground, edgeInset: .init(vertical: 7.5, horizontal: 12), borderColor: .clear, borderWidth: 0, cornerRadius: 12)
+		}.addToView(stack)
+		stack.addArrangedSubview(.spacer())
+		[label, stack].addToView(quickActions)
+		
 	}
 	
 	public func configure(with model: EmptyModel) {
